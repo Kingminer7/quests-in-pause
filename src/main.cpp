346c2007664f3f95170951a553035ba297c21042
km7dev <n0tkingminer7@gmail.com>
2024-08-21 22:05:33 -0400

    create the mod

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <Geode/modify/PauseLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(ModifiedPause, PauseLayer) {
+	void customSetup() {
+		PauseLayer::customSetup();
+
+		CCNode * sprite = CircleButtonSprite::createWithSpriteFrameName("quests.png"_spr, .8f, CircleBaseColor::Green, CircleBaseSize::MediumAlt);
+		sprite->setScale(0.6);
+
+		auto questButton = CCMenuItemSpriteExtra::create(
+			sprite,
+			this,
+			menu_selector(ModifiedPause::onQuests)
+		);
+		
+
+		auto menu = this->getChildByID("right-button-menu");
+		menu->addChild(questButton);
+
+		questButton->setID("quests-button"_spr);
+
+		menu->updateLayout();
+	}
+
+	void onQuests(CCObject*) {
+		ChallengesPage::create()->show();
+	}
+};
