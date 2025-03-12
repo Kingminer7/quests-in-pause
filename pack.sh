2ae2ca298e14f23e0d9aec589de2ca14a59bacae
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2025-03-12 18:13:21 -0400

    oh yeah i messed this up because i set my env var wrong

--- pack.sh
+++ pack.sh
@@ -1,2 +1,2 @@
 zip -r km7dev.quests_in_pause_menu.zip . -x pack.sh -x km7dev.quests_in_pause_menu.zip
-cp km7dev.quests_in_pause_menu.zip $GEOMETRY_DASH
+cp km7dev.quests_in_pause_menu.zip $GEOMETRY_DASH/geode/config/geode.texture-loader/packs/
